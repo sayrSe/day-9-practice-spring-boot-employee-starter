583a533a569c0c6f271830c61169afeb937fb13a
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-24 19:23:34 +0800

    feat: use CompanyJPARepository in CompanyService and change findById method return value

--- src/main/java/com/afs/restapi/service/CompanyService.java
+++ src/main/java/com/afs/restapi/service/CompanyService.java
@@ -1,63 +1,53 @@
 package com.afs.restapi.service;
 
 import com.afs.restapi.entity.Company;
-import com.afs.restapi.exception.CompanyNotFoundException;
-import com.afs.restapi.repository.InMemoryCompanyRepository;
 import com.afs.restapi.entity.Employee;
-import com.afs.restapi.repository.InMemoryEmployeeRepository;
+import com.afs.restapi.exception.CompanyNotFoundException;
+import com.afs.restapi.repository.CompanyJPARepository;
+import com.afs.restapi.repository.EmployeeJPARepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CompanyService {
 
-    private final InMemoryCompanyRepository inMemoryCompanyRepository;
-    private final InMemoryEmployeeRepository inMemoryEmployeeRepository;
+    private final CompanyJPARepository companyJPARepository;
+    private final EmployeeJPARepository employeeJPARepository;
 
-    public CompanyService(InMemoryCompanyRepository inMemoryCompanyRepository, InMemoryEmployeeRepository inMemoryEmployeeRepository) {
-        this.inMemoryCompanyRepository = inMemoryCompanyRepository;
-        this.inMemoryEmployeeRepository = inMemoryEmployeeRepository;
-    }
-
-    public InMemoryCompanyRepository getCompanyRepository() {
-        return inMemoryCompanyRepository;
-    }
-
-    public InMemoryEmployeeRepository getEmployeeRepository() {
-        return inMemoryEmployeeRepository;
+    public CompanyService(CompanyJPARepository companyJPARepository, EmployeeJPARepository employeeJPARepository) {
+        this.companyJPARepository = companyJPARepository;
+        this.employeeJPARepository = employeeJPARepository;
     }
 
     public List<Company> findAll() {
-        return getCompanyRepository().getCompanies();
+        return companyJPARepository.findAll();
     }
 
     public List<Company> findByPage(Integer pageNumber, Integer pageSize) {
-        return getCompanyRepository().findByPage(pageNumber, pageSize);
+        return companyJPARepository.findAll(PageRequest.of(pageNumber - 1, pageSize)).toList();
     }
 
     public Company findById(Long id) {
-        Company company = getCompanyRepository().findById(id).orElseThrow(CompanyNotFoundException::new);
-        List<Employee> employees = getEmployeeRepository().findByCompanyId(company.getId());
-        company.setEmployees(employees);
-        return company;
+        return companyJPARepository.findById(id).orElseThrow(CompanyNotFoundException::new);
     }
 
     public void update(Long id, Company company) {
         Company toBeUpdatedCompany = findById(id);
         toBeUpdatedCompany.setName(company.getName());
+        companyJPARepository.save(toBeUpdatedCompany);
     }
 
     public Company create(Company company) {
-        return getCompanyRepository().insert(company);
+        return companyJPARepository.save(company);
     }
 
     public List<Employee> findEmployeesByCompanyId(Long id) {
-        return getEmployeeRepository().findByCompanyId(id);
+        return employeeJPARepository.findByCompanyId(id);
     }
 
     public void delete(Long id) {
-        inMemoryCompanyRepository.deleteById(id);
+        companyJPARepository.deleteById(id);
     }
 }
