8bb57c3b3f16f973857260c7af22f5d592ec5ddf
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-24 21:21:25 +0800

    test: case for should_return_the_employee_when_findById_given_employee_jpa_service_and_an_employee_id

--- src/test/java/com/afs/restapi/service/EmployeeServiceTest.java
+++ src/test/java/com/afs/restapi/service/EmployeeServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -38,5 +39,22 @@ class EmployeeServiceTest {
         assertEquals(allEmployees.get(0).getAge(), employee.getAge());
         assertEquals(allEmployees.get(0).getGender(), employee.getGender());
         assertEquals(allEmployees.get(0).getSalary(), employee.getSalary());
+    }
+
+    @Test
+    void should_return_the_employee_when_findById_given_employee_jpa_service_and_an_employee_id() {
+        // Given
+        Employee employee = new Employee(1L, "Lucy", 20, "Female", 3000);
+        when(mockedEmployeeJPARepository.findById(employee.getId())).thenReturn(Optional.of(employee));
+
+        // When
+        Employee foundEmployee = employeeService.findById(employee.getId());
+
+        // Then
+        assertEquals(employee.getId(), foundEmployee.getId());
+        assertEquals(employee.getName(), foundEmployee.getName());
+        assertEquals(employee.getAge(), foundEmployee.getAge());
+        assertEquals(employee.getGender(), foundEmployee.getGender());
+        assertEquals(employee.getSalary(), foundEmployee.getSalary());
     }
 }
